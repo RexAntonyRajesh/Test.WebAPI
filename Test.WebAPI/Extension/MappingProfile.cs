c0dce2340a89aba8ba023138e1961315ba683953
Antony Rajesh <Antony.Rajesh@thomsonreuters.com>
2023-11-24 22:21:42 +0530

    Add project files.

---
+++ Test.WebAPI/Extension/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Test.Domain;
+using Test.Service;
+
+namespace TestAPI.Extension
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserResponse>();
+
+            CreateMap<Role, RoleResponse>();
+        }
+    }
+}
